d69d5c6b0cca52def3681e64ec7dbbc00b172f36
d814394 <irfan.elahi@team.telstra.com>
2017-07-05 16:32:26 +1000

    adding a code about removing header and trailer using zipWithIndex in Spark using Scala

---
+++ removing_header_trailer.scala
@@ -0,0 +1,9 @@
+//removing header and trailer using zipWithIndex:
+  def stripLines(headerLines:Int,footerLines:Int,filename:String,delimiter:String)={
+
+      val file_rdd=sc.textFile(filename)
+      val file_rdd_length=file_rdd.count
+      val file_rddx=file_rdd.zipWithIndex.map{case(pcontent,pindex)=>(pindex,pcontent)}.filter{case(pindex,pcontent)=>{if (headerLines>0) !((0 to (headerLines-1)).toList.contains(pindex)) else true} && 
+{if (footerLines>0) !((file_rdd_length to (file_rdd_length-footerLines) by -1).toList.contains(pindex)) else true}}.map{case(pindex,pcontent)=>pcontent.split(delimiter)}
+    file_rddx
+  }
